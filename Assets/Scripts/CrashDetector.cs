781a395fdc62295a72387f23f6977da9f2f36c31
BeyondTheThreshold <rlgns4786@naver.com>
2025-07-29 16:06:45 +0900

    Add Particle Effect
    Add Particle Effect
    if character crash the road and finsh the line, trigger Particle Effect

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -4,14 +4,20 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] ParticleSystem crashEffect;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Road")
         {
+            crashEffect.Play();
             Debug.Log("replay!");
-            SceneManager.LoadScene(0);
+            Invoke("ReloadScene", 0.2f);
         }
     }
-          
+          void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+  
       
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -4,11 +4,19 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float MagicNumber = 2f;
+    [SerializeField] ParticleSystem finshEffect;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ch") // 4분59초
         {
-            SceneManager.LoadScene(0);
+            finshEffect.Play();
+            Debug.Log("Finsh!");
+            Invoke("ReloadScene", MagicNumber);
         }
+    }
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
