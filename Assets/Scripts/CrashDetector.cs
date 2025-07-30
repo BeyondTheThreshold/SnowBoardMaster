c98cfe6f1ad1f3c5b2e10993b40040839d76dd98
BeyondTheThreshold <rlgns4786@naver.com>
2025-07-30 17:36:10 +0900

    last update
    last update!

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -6,13 +6,15 @@ public class CrashDetector : MonoBehaviour
 {
     [SerializeField] ParticleSystem crashEffect;
     [SerializeField] AudioClip crash;
+    [SerializeField] bool canCrash = true;
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Road")
+        if (other.tag == "Road"&& canCrash == true)
         {
             crashEffect.Play();
             GetComponent<AudioSource>().PlayOneShot(crash);
             Debug.Log("replay!");
+            canCrash = false;
             Invoke("ReloadScene", 1.5f);
         }
     }
